e06d48e7bd70b9f2e3b32cbbe8625e087a3bea7e
FullViper2406 <119512140+FullViper2406@users.noreply.github.com>
2023-03-23 09:59:15 -0300

    atualizar telas

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -35,11 +35,13 @@ namespace JobStack
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(156, 203);
+            this.button1.Location = new System.Drawing.Point(156, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -58,7 +60,7 @@ namespace JobStack
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 49);
+            this.label1.Location = new System.Drawing.Point(189, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 2;
@@ -86,16 +88,37 @@ namespace JobStack
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Location = new System.Drawing.Point(66, 40);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 10;
+            this.button2.Text = "Voltar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Senha";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(304, 299);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -117,6 +140,8 @@ namespace JobStack
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Login.cs
+++ Login.cs
@@ -16,55 +16,90 @@ namespace JobStack
         public Login()
         {
             InitializeComponent();
+            // Esconder o campo de senha
 
             textBox2.Visible = false;
+            // Desabilitar o campo de senha
 
             textBox2.Enabled = false;
+            // Esconder a label de aviso
 
             label2.Visible = false;
+            // Adicionar evento de clique ao botão
             button1.Click += Button1_Click1;
-          
+            // Esconder a senha digitada com asteriscos
+
             textBox2.PasswordChar = '*';
+            // Esconder o botão 
+
+            button2.Visible = false;
+            // Esconder as labels 
+
+            label3.Visible = false;
+            label4.Visible = false;
+            // Mudar o texto do botão para "Próximo"
+
+            button1.Text = "Próximo";
 
 
         }
+        // Variável de controle para saber se o botão já foi clicado antes
 
         private bool pvez = true;
 
 
-    private void Button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
 
 
         }
         private void Button1_Click1(object sender, EventArgs e)
-        {
+        {    // Obter o email digitado pelo usuário
+
             string email = textBox1.Text;
             Console.WriteLine("Email: " + email);
 
-            
-            int usuario = BancodeDados.Login(email);
+            // Buscar o ID do usuário no banco de dados
+
+            int id = BancodeDados.BuscarID(email);
+            // Verificar se o campo de email está vazio
+
             if (textBox1.Text == "")
             {
-                MessageBox.Show("Campo email vazio");
+                label4.Visible = true;
+
+                label4.Text = "Campo email vazio";
 
             }
-          
-             else if (usuario == 0)
+            // Verificar se o usuário não está cadastrado
+
+            else if (id == 0)
+
             {
-                MessageBox.Show("Usuario nao cadastrado");
+                label4.Visible = true;
+
+                label4.Text = "Usuario não cadastrado";
 
             }
+            // Se for a primeira vez que o botão é clicado, exibir o campo de senha e mudar o texto do botão
+
             else
+
             {
                 if (pvez)
                 {
+                    label4.Visible = false;
+                    button1.Text = "Entrar";
+                    // Chamar o método Email para exibir informações sobre o usuário
+
                     Email();
+                    // Remover o evento de clique anterior e adicionar o novo evento
 
                     button1.Click -= Button1_Click1;
 
                     button1.Click += Button1_Click2;
 
+                    // Atualizar a variável de controle para saber que o botão já foi clicado uma vez
 
                     pvez = false;
 
@@ -79,8 +114,11 @@ namespace JobStack
         private void Button1_Click2(object sender, EventArgs e)
         {
 
+            // Chamar o método Senha para validar a senha digitada
 
             Senha();
+            // Se não for a primeira vez que o botão é clicado, remover o evento de clique atual e adicionar o evento anterior
+
             if (!pvez)
             {
 
@@ -96,252 +134,202 @@ namespace JobStack
 
 
             }
-      
+
 
 
         }
+        // Variável de controle para saber se as informações do usuário já foram exibidas
+
         private bool Emailm = false;
         private void Email()
         {
+            // Obter o email digitado pelo usuário
 
             string email = textBox1.Text;
             Console.WriteLine("Email: " + email);
+            // Buscar o ID do usuário no banco de dados
 
+            int id = BancodeDados.BuscarID(email);
+            // Exibir o email do usuário na label
 
+            label1.Text = textBox1.Text;
+            // Desabilita e esconde a primeira caixa de texto
 
-            int usuario = BancodeDados.Login(email);
-            switch (usuario)
+            textBox1.Enabled = false;
+
+            textBox1.Visible = false;
+            // Mostra a segunda caixa de texto, o rótulo e o botão
+
+            label2.Visible = true;
+            textBox2.Visible = true;
+            textBox2.Enabled = true;
+            button2.Visible = true;
+            // Identifica o tipo de usuário com base em seu ID
+
+            if (id / 1000 > 0 && id / 1000 < 6)
             {
-                case 1:
-                    label1.Enabled = false;
-                    textBox1.Enabled = false;
-                    label1.Visible = false;
-                    textBox1.Visible = false;
-                    label2.Visible = true;
-                    textBox2.Visible = true;
-                    textBox2.Enabled = true;
-                    if (!Emailm)
-                    {
-                        MessageBox.Show("Usuario é um aluno");
-                        Emailm = true;
-                    }
-                    break;
-                case 2:
-                    label1.Enabled = false;
-                    textBox1.Enabled = false;
-                    label1.Visible = false;
-                    textBox1.Visible = false;
-                    label2.Visible = true;
-                    textBox2.Visible = true;
-                    textBox2.Enabled = true;
 
-                    if (!Emailm)
-                    {
-                        MessageBox.Show("Usuario é uma empresa");
-                        Emailm = true;
-                    }
+                // Se for um aluno, exibe uma mensagem
 
-                    break;
-                case 3:
-                    label1.Enabled = false;
-                    textBox1.Enabled = false;
-                    label1.Visible = false;
-                    textBox1.Visible = false;
-                    label2.Visible = true;
-                    textBox2.Visible = true;
-                    textBox2.Enabled = true;
+                if (!Emailm)
+                {
+                    MessageBox.Show("Usuario é um aluno");
+                    Emailm = true;
+                }
+            }
+            else if (id / 1000 > 5)
 
-                    if (!Emailm)
-                    {
-                        MessageBox.Show("Usuario é um Admin");
-                        Emailm = true;
-                    }
+            {
 
-                    break;
-                case 4:
-                    label1.Enabled = false;
-                    textBox1.Enabled = false;
-                    label1.Visible = false;
-                    textBox1.Visible = false;
-                    label2.Visible = true;
-                    textBox2.Visible = true;
-                    textBox2.Enabled = true;
+                // Se for uma empresa, exibe uma mensagem
 
-                    if (!Emailm)
-                    {
-                        MessageBox.Show("Usuario é um Coordenador");
-                        Emailm = true;
-                    }
-
-                    break;
-
+                if (!Emailm)
+                {
+                    MessageBox.Show("Usuario é uma empresa");
+                    Emailm = true;
+                }
 
             }
-        }
 
 
-        private void Senha()
-        {
-            button1.Click -= Button1_Click1;
 
-            button1.Click += Button1_Click2;
-            button1.Click -= Button1_Click2;
-
-            if (textBox2.Text == "")
+            else if (id / 100 == 1)
             {
-                MessageBox.Show(" campo senha vazio");
+                // Se for um coordenador, exibe uma mensagem
+
+                if (!Emailm)
+                {
+                    MessageBox.Show("Usuario é um Coordenador");
+                    Emailm = true;
+                }
             }
             else
             {
-                string email = textBox1.Text;
+                // Se for um admin, exibe uma mensagem
 
-
-
-
-
-
-
-                int usuario = BancodeDados.Login(email);
-
-
-                switch (usuario)
+                if (!Emailm)
                 {
-                    case 1:
-                        label1.Enabled = false;
-                        textBox1.Enabled = false;
-                        label1.Visible = false;
-                        textBox1.Visible = false;
-                        label2.Visible = true;
-                        textBox2.Visible = true;
-                        textBox2.Enabled = true;
-
-                        if (BancodeDados.BuscarAluno(email).GetSenha().Equals(textBox2.Text))
-                        {
-
-                            MessageBox.Show("sucesso");
-                            Aluno1 p = new Aluno1();
-                            p.ShowDialog();
-                        }
-
-                        else
-                        {
-                            label1.Enabled = false;
-                            textBox1.Enabled = false;
-                            label1.Visible = false;
-                            textBox1.Visible = false;
-                            label2.Visible = true;
-                            textBox2.Visible = true;
-                            textBox2.Enabled = true;
-
-                            MessageBox.Show("erro");
-
-                        }
-
-                        break;
-                    case 2:
-                        label1.Enabled = false;
-                        textBox1.Enabled = false;
-                        label1.Visible = false;
-                        textBox1.Visible = false;
-                        label2.Visible = true;
-                        textBox2.Visible = true;
-                        textBox2.Enabled = true;
-
-                        if (BancodeDados.BuscarEmpresa(email).GetSenha().Equals(textBox2.Text))
-                        {
-
-                            MessageBox.Show("sucesso");
-                            Aluno1 p = new Aluno1();
-                            p.ShowDialog();
-                        }
-                        else if (textBox2.Text == "")
-                        {
-                            MessageBox.Show(" campo senha vazio");
-                        }
-                        else
-                        {
-                            label1.Enabled = false;
-                            textBox1.Enabled = false;
-                            label1.Visible = false;
-                            textBox1.Visible = false;
-                            label2.Visible = true;
-                            textBox2.Visible = true;
-                            textBox2.Enabled = true;
-                            MessageBox.Show("erro");
-
-                        }
-
-                        break;
-                    case 3:
-                        label1.Enabled = false;
-                        textBox1.Enabled = false;
-                        label1.Visible = false;
-                        textBox1.Visible = false;
-                        label2.Visible = true;
-                        textBox2.Visible = true;
-                        textBox2.Enabled = true;
-                        if (BancodeDados.BuscarAdmin(email).GetSenha().Equals(textBox2.Text))
-                        {
-
-                            MessageBox.Show("sucesso");
-                            Aluno1 p = new Aluno1();
-                            p.ShowDialog();
-                        }
-                        else if (textBox2.Text == "")
-                        {
-                            MessageBox.Show(" campo senha vazio");
-                        }
-                        else
-                        {
-                            label1.Enabled = false;
-                            textBox1.Enabled = false;
-                            label1.Visible = false;
-                            textBox1.Visible = false;
-                            label2.Visible = true;
-                            textBox2.Visible = true;
-                            textBox2.Enabled = true;
-                            MessageBox.Show("erro");
-
-                        }
-
-                        break;
-                    case 4:
-
-                        label1.Enabled = false;
-                        textBox1.Enabled = false;
-                        label1.Visible = false;
-                        textBox1.Visible = false;
-                        label2.Visible = true;
-                        textBox2.Visible = true;
-                        textBox2.Enabled = true;
-                        if (BancodeDados.BuscarCoordenador(email).GetSenha().Equals(textBox2.Text))
-                        {
-
-                            MessageBox.Show("sucesso");
-                            Aluno1 p = new Aluno1();
-                            p.ShowDialog();
-                        }
-                        else if (textBox2.Text == "")
-                        {
-                            MessageBox.Show(" campo senha vazio");
-                        }
-                        else
-                        {
-                            label1.Enabled = false;
-                            textBox1.Enabled = false;
-                            label1.Visible = false;
-                            textBox1.Visible = false;
-                            label2.Visible = true;
-                            textBox2.Visible = true;
-                            textBox2.Enabled = true;
-                            MessageBox.Show("erro");
-
-                        }
-
-                        break;
+                    MessageBox.Show("Usuario é um Admin");
+                    Emailm = true;
                 }
+
+            }
+
+        }
+
+
+
+        private void Senha()
+        {    // Desassocia o evento Button1_Click1 do botão button1
+
+            button1.Click -= Button1_Click1;
+            // Associa o evento Button1_Click2 ao botão button1
+
+            button1.Click += Button1_Click2;
+            // Desassocia o evento Button1_Click2 do botão button1
+
+            button1.Click -= Button1_Click2;
+            // Define o texto do label1 como o texto inserido no textBox1
+
+            label1.Text = textBox1.Text;
+            // Torna o textBox1 invisível
+
+            textBox1.Visible = false;
+            // Torna o label2 visível
+
+            label2.Visible = true;
+            // Torna o textBox2 visível e habilitado
+
+            textBox2.Visible = true;
+            textBox2.Enabled = true;
+            // Obtém o e-mail e a senha inseridos nos textBoxes
+
+            string email = textBox1.Text;
+            string senha = textBox2.Text;
+            // Obtém o ID do usuário a partir do e-mail
+
+            int id = BancodeDados.BuscarID(email);
+            // Verifica se o campo senha está vazio e exibe uma mensagem de erro caso esteja
+
+            if (textBox2.Text == "")
+            {
+                label3.Visible = true;
+                label3.Text = "Campo senha vazio";
+
+            }
+            // Verifica se a senha inserida está incorreta e exibe uma mensagem de erro caso esteja
+
+            else if (!BancodeDados.Login(id, senha))
+            {
+                label3.Visible = true;
+                label3.Text = "Senha incorreta";
+            }
+
+
+
+
+
+            // Verifica se o usuário é um aluno e exibe a janela do formulário Aluno1
+
+            else if (id / 1000 > 0 && id / 1000 < 6)
+            {
+
+                this.Hide();
+                Aluno1 p = new Aluno1();
+
+                p.ShowDialog();
+            }
+
+
+
+
+
+            // Verifica se o usuário é uma empresa e exibe a janela do formulário Empresa1
+
+            else if (id / 1000 > 5)
+            {
+                this.Hide();
+
+                Empresa1 p = new Empresa1();
+                p.ShowDialog();
+            }
+
+
+            // Verifica se o usuário é um coordenador e exibe a janela do formulário Coordenador1
+
+            else if (id / 100 == 1)
+            {
+
+
+                this.Hide();
+
+                Coordenador1 p = new Coordenador1();
+                p.ShowDialog();
+            }
+
+
+            else
+            {
+
+                this.Hide();
+
+                Admin1 p = new Admin1();
+                p.ShowDialog();
             }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -363,7 +351,7 @@ namespace JobStack
         private void TextBox2_TextChanged(object sender, EventArgs e)
         {
 
-      
+
 
         }
 
@@ -378,6 +366,22 @@ namespace JobStack
 
         }
 
-       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button1_Click1(sender, e);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
+
