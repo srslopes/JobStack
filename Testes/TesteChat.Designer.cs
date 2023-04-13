4eb7644bbe07e540d377fb172c0348c3c687c460
Samuel Lopes <116354478+srslopes@users.noreply.github.com>
2023-04-13 10:27:57 -0300

    Teste do Chat #2

--- Testes/TesteChat.Designer.cs
+++ Testes/TesteChat.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.panelConversas = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelChat = new System.Windows.Forms.Panel();
+            this.buttonEnviar = new System.Windows.Forms.Button();
+            this.textBoxMensagem = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // panelConversas
@@ -41,37 +41,37 @@
             this.panelConversas.Size = new System.Drawing.Size(200, 426);
             this.panelConversas.TabIndex = 0;
             // 
-            // panel1
+            // panelChat
             // 
-            this.panel1.Location = new System.Drawing.Point(218, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 388);
-            this.panel1.TabIndex = 1;
+            this.panelChat.Location = new System.Drawing.Point(218, 12);
+            this.panelChat.Name = "panelChat";
+            this.panelChat.Size = new System.Drawing.Size(570, 388);
+            this.panelChat.TabIndex = 1;
             // 
-            // button1
+            // buttonEnviar
             // 
-            this.button1.Location = new System.Drawing.Point(681, 406);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 32);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonEnviar.Location = new System.Drawing.Point(681, 406);
+            this.buttonEnviar.Name = "buttonEnviar";
+            this.buttonEnviar.Size = new System.Drawing.Size(107, 32);
+            this.buttonEnviar.TabIndex = 3;
+            this.buttonEnviar.Text = "Enviar";
+            this.buttonEnviar.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBoxMensagem
             // 
-            this.textBox1.Location = new System.Drawing.Point(218, 413);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(457, 20);
-            this.textBox1.TabIndex = 4;
+            this.textBoxMensagem.Location = new System.Drawing.Point(218, 413);
+            this.textBoxMensagem.Name = "textBoxMensagem";
+            this.textBoxMensagem.Size = new System.Drawing.Size(457, 20);
+            this.textBoxMensagem.TabIndex = 4;
             // 
             // TesteChat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBoxMensagem);
+            this.Controls.Add(this.buttonEnviar);
+            this.Controls.Add(this.panelChat);
             this.Controls.Add(this.panelConversas);
             this.Name = "TesteChat";
             this.Text = "TesteChat";
@@ -83,8 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Panel panelConversas;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panelChat;
+        private System.Windows.Forms.Button buttonEnviar;
+        private System.Windows.Forms.TextBox textBoxMensagem;
     }
 }
