c3566ed082caff5a071fa577ac9481ebc434e654
sam <116354478+srslopes@users.noreply.github.com>
2023-06-16 08:56:03 -0300

    Update TVagaCompletaCoordenador.xaml.cs

--- TVagaCompletaCoordenador.xaml.cs
+++ TVagaCompletaCoordenador.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +19,58 @@ namespace JobStack
     /// </summary>
     public partial class TVagaCompletaCoordenador : Window
     {
-        public TVagaCompletaCoordenador()
+        private Vaga vaga;
+        public TVagaCompletaCoordenador(int id)
         {
             InitializeComponent();
+            vaga = BancodeDados.BuscarVaga(id);
+            AttDados();
+        }
+
+        public void AttDados()
+        {
+            TituloVaga.Text = vaga.GetNome();
+            NomeEmpresa.Text = BancodeDados.BuscarEmpresa(vaga.GetIdEmpresa()).GetNome();
+            DescricaoVaga.Text = vaga.GetDescricao();
+            TipoVaga.Text = vaga.GetTipo();
+            Turno.Text = vaga.GetTurno();
+            Jornada.Text = vaga.GetJornada();
+            QtdeVaga.Text = vaga.GetNVagas().ToString();
+            switch (vaga.GetCurso())
+            {
+                case 0:
+                    Curso.Text = "Análise e Desenvolvimento de Sistemas";
+                    break;
+                case 1:
+                    Curso.Text = "Gestão Empresarial";
+                    break;
+                case 2:
+                    Curso.Text = "Eventos";
+                    break;
+                case 3:
+                    Curso.Text = "Gestão de Produção Industrial";
+                    break;
+            }
         }
 
         private void BtnFechar_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            TSolicitacoesVagasCoordenador analisar = new TSolicitacoesVagasCoordenador();
+            BancodeDados.MenuCoordenador.CarregarJanela(analisar);
+        }
+
+        private void BtnAceitar_Click(object sender, RoutedEventArgs e)
+        {
+            vaga.Aprovar();
+            TSolicitacoesVagasCoordenador analisar = new TSolicitacoesVagasCoordenador();
+            BancodeDados.MenuCoordenador.CarregarJanela(analisar);
+        }
+
+        private void BtnRejeitar_Click(object sender, RoutedEventArgs e)
+        {
+            vaga.Rejeitar();
+            TSolicitacoesVagasCoordenador analisar = new TSolicitacoesVagasCoordenador();
+            BancodeDados.MenuCoordenador.CarregarJanela(analisar);
         }
     }
 }
